0be0ba311b4f1ffc6a40edd4b9e1ccc5e9cd53a0
aide <aideghdzcontacto@gmail.com>
2020-02-05 10:48:16 -0600

    The users pulate with example data

--- src/app/views/users/user-list.component.ts
+++ src/app/views/users/user-list.component.ts
@@ -17,8 +17,31 @@ export class UserListComponent  {
 
   @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
 
+  loadingIndicator = true;
   rows = [];
   selected = [];
-  constructor( private appService: AppService) {}
+  constructor( private appService: AppService) {
+    this.fetch((data) => {
+      // cache our list
+      this.temp = [...data];
+
+      // push our inital complete list
+      this.rows = data;
+
+      setTimeout(() => { this.loadingIndicator = false; }, 1500);
+   });
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/json/ngx-datatable-data.json`);
+
+    req.onload = () => {
+       const data = JSON.parse(req.response);
+       cb(data);
+    };
+
+    req.send();
+ }
   
 }
