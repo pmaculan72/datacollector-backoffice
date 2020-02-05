6e4fe35293982c6c010d02bc1df86d1aab35d133
aide <aideghdzcontacto@gmail.com>
2020-02-04 19:20:24 -0600

    Change button label

--- src/app/views/users/user-list.component.ts
+++ src/app/views/users/user-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,  ViewChild } from '@angular/core';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { NewCompanyComponent } from 'src/app/components/new-company/new-company.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'user-list',
-  templateUrl: 'user-list.template.html'
+  templateUrl: 'user-list.template.html',
+  styleUrls: [
+    '../../../../node_modules/@swimlane/ngx-datatable/index.scss',
+    '../../../vendor/libs/ngx-datatable/ngx-datatable.scss',
+    '../../../../node_modules/@swimlane/ngx-datatable/assets/icons.css'
+ ]
 })
 export class UserListComponent  {
+
+  @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
+
+  rows = [];
   constructor() {}
   
 }
